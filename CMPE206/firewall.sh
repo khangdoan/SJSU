30c66b942177b07d96c3f6ccac4a77379624d711
Khang K. Doan <khang.doan@sjsu.edu>
2019-11-21 10:42:36 -0800

    Create firewall.sh

---
+++ CMPE206/firewall.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#Clear all rules
+iptables -F
+
+#Allow PING for everyone
+iptables -A FORWARD -p icmp -j ACCEPT
+
+# allow HHTP/HHTPS for WIFI lan
+iptables -A FORWARD -p tcp --dport 80 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 443 -j ACCEPT
+
+#block HTTP/HTTPS from sjsu.edu and redirect to webproxy
+# sjsu.edu -> 130.65.255.101
+iptables -t nat -A OUTPUT -p tcp --dport 80  -d 130.65.255.101 -j DNAT --to-destination <web proxy's ip>
+iptables -t nat -A OUTPUT -p tcp --dport 443  -d 130.65.255.101 -j DNAT --to-destination <web proxy's ip>
